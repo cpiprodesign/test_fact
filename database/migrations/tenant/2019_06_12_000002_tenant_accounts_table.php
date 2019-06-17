bc0950b76dc445a22126ba63f5a6de16b2d5822b
Aurora Soledad <aguerra95ugas@gmail.com>
2019-06-17 10:32:57 -0500

    Se agrego caja general como cuenta registrada por defecto

--- database/migrations/tenant/2019_06_12_000002_tenant_accounts_table.php
+++ database/migrations/tenant/2019_06_12_000002_tenant_accounts_table.php
@@ -28,6 +28,11 @@ class TenantAccountsTable extends Migration
             $table->foreign('currency_type_id')->references('id')->on('cat_currency_types');
             $table->foreign('account_type_id')->references('id')->on('cat_account_types');
         });
+
+        DB::table('accounts')->insert([
+            ['name' => 'Caja General', 'number' => '-', 'currency_type_id' => 'PEN', 'account_type_id' => 3, 'date_of_issue' => '2019-06-13',
+            'beginning_balance' => 0, 'current_balance' => 0, 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ]);
     }
 
     /**
