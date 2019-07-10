96076ef4d3a004c8e81a79ef2dc085d9cc36f38d
Aurora Soledad <aguerra95ugas@gmail.com>
2019-07-10 12:31:05 -0500

    js

--- resources/views/tenant/layouts/app.blade.php
+++ resources/views/tenant/layouts/app.blade.php
@@ -139,7 +139,7 @@
 
     @stack('scripts')
 
-    <script src="{{ asset('js/app.js?v=20190709') }}"></script>
+    <script src="{{ asset('js/app.js?v=20190710') }}"></script>
     <!-- Theme Base, Components and Settings -->
     <script src="{{asset('porto-light/js/theme.js')}}"></script>
     <script src="//code.tidio.co/8txsjuzw2uthkcsbyrwynuffhsdzpbwi.js"></script>
