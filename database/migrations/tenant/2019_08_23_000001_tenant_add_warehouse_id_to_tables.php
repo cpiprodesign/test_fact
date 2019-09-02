8b794534f29b4c82a9f5b6b156968c15d70ed812
Aurora Soledad <aguerra95ugas@gmail.com>
2019-09-02 01:38:19 -0500

    solucionado migracion de columna warehouse

--- database/migrations/tenant/2019_08_23_000001_tenant_add_warehouse_id_to_tables.php
+++ database/migrations/tenant/2019_08_23_000001_tenant_add_warehouse_id_to_tables.php
@@ -15,22 +15,31 @@ class TenantAddWarehouseIdToTables extends Migration
     {
         Schema::table('documents', function (Blueprint $table) {
             $table->unsignedInteger('warehouse_id')->after('establishment_id');
-            $table->foreign('warehouse_id')->references('id')->on('warehouses');
         });
 
         Schema::table('purchases', function (Blueprint $table) {
             $table->unsignedInteger('warehouse_id')->after('establishment_id');
-            $table->foreign('warehouse_id')->references('id')->on('warehouses');
         });
 
         Schema::table('sale_notes', function (Blueprint $table) {
             $table->unsignedInteger('warehouse_id')->after('establishment_id');
-            $table->foreign('warehouse_id')->references('id')->on('warehouses');
         });
         
         DB::statement("UPDATE documents SET establishment_id = warehouse_id");
         DB::statement("UPDATE purchases SET establishment_id = warehouse_id");
         DB::statement("UPDATE sale_notes SET establishment_id = warehouse_id");
+
+        Schema::table('documents', function (Blueprint $table) {
+            $table->foreign('warehouse_id')->references('id')->on('warehouses');
+        });
+
+        Schema::table('purchases', function (Blueprint $table) {
+            $table->foreign('warehouse_id')->references('id')->on('warehouses');
+        });
+
+        Schema::table('sale_notes', function (Blueprint $table) {
+            $table->foreign('warehouse_id')->references('id')->on('warehouses');
+        });
     }
 
     /**
