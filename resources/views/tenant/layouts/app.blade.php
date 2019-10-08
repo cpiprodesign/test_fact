34cc2108430d84157796be10464424aee46c1e24
Aurora Soledad <aguerra95ugas@gmail.com>
2019-10-08 13:56:09 -0500

    forzar js

--- resources/views/tenant/layouts/app.blade.php
+++ resources/views/tenant/layouts/app.blade.php
@@ -143,7 +143,7 @@
 
     @stack('scripts')
 
-    <script src="{{ asset('js/app.js?v=20190910') }}"></script>
+    <script src="{{ asset('js/app.js?v=20191008') }}"></script>
     <!-- Theme Base, Components and Settings -->
     <script src="{{asset('porto-light/js/theme.js')}}"></script>
     <script src="//code.tidio.co/8txsjuzw2uthkcsbyrwynuffhsdzpbwi.js"></script>
